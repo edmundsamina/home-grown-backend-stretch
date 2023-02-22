c5449528966f5f59aef271012530e91b390b29e1
willbyrne <will.bl.byrne@gmail.com>
2023-02-22 14:32:41 +0000

    basic setup kind of working

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-import express from 'express'
-import { router } from './routers/index.js'
-import cors from 'cors'
-import decodeToken from './middleware/index.js'
-import { publicrouter } from './routers/public.js'
-const app = express()
+import express from "express";
+import { router } from "./routers/index.js";
+import cors from "cors";
+import decodeToken from "./middleware/index.js";
+import { publicrouter } from "./routers/public.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const app = express();
+const httpServer = createServer(app);
+const socketIO = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+app.use(cors());
+app.use(express.json());
+socketIO.on("connection", (socket) => {
+  console.log("user connected");
+  socketIO.emit("messageResponse", "hello");
+  socket.on("disconnect", () => {
+    console.log("disconnection");
+    socketIO.emit("messageResponse", () => {
+      console.log("bye");
+    });
+  });
+});
 
-app.use(cors())
-app.use(express.json())
 app.use("/api/homegrown/public/", publicrouter);
-app.use(decodeToken)
+app.use(decodeToken);
 app.use("/api/homegrown/", router);
 
-  
-
-export default app 
+httpServer.listen(3001);
+export default app;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT ?? 3000;
-
+const PORT = process.env.PORT ?? 4000;
 
 app.listen(PORT, function () {
   console.log(`Server is listening on http://localhost:${PORT}`);
